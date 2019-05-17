12ef54467ad01b4b9a7e0615e8721fea8315573e
vega089 <50191391+vega089@users.noreply.github.com>
2019-05-16 22:48:46 -0700

    merged a folder

---
+++ New folder/project.js
@@ -0,0 +1,15 @@
+// ------------------------------------------------------------------
+// JOVO PROJECT CONFIGURATION
+// ------------------------------------------------------------------
+
+module.exports = {
+    alexaSkill: {
+       nlu: 'alexa',
+        skillId: 'amzn1.ask.skill.afc19e6e-35c7-4b55-8af7-69bf4c40f657'
+    },
+    googleAction: {
+       nlu: 'dialogflow',
+    },
+    endpoint: '${JOVO_WEBHOOK_URL}',
+};
+ 
